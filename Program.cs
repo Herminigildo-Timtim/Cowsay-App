e5ee6f12bfc00cef180c9b273e58fc3336d0af4e
Herminigildo L. Timtim <hermitimtim23@gmail.com>
2025-02-12 03:21:43 +0800

    File split

--- Program.cs
+++ Program.cs
@@ -1,48 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
- 
-static class CowsayApp
-{
-    public static void Say(string message)
-    {
-        try
-        {
-            ProcessStartInfo psi = new()
-            {
-                FileName = "/usr/games/cowsay", 
-                Arguments = message, 
-                RedirectStandardOutput = true,
-                RedirectStandardError = true, 
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-            using Process? process = Process.Start(psi);
-
-            if (process == null)
-            {
-                Console.WriteLine("Error: Unable to start cowsay process.");
-                return;
-            }
-
-            string output = process.StandardOutput.ReadToEnd();
-            string error = process.StandardError.ReadToEnd();
-
-            if (!string.IsNullOrEmpty(error))
-            {
-                Console.WriteLine($"Error: {error}");
-            }
-            else
-            {
-                Console.WriteLine(output);
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"An error occurred: {ex.Message}");
-        }
-    }
-}
 
 class Program
 {
--- obj/Debug/net9.0/cowsay.AssemblyInfo.cs
+++ obj/Debug/net9.0/cowsay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cowsay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dfae2e45fd97de572f8729e71fab952f04eddf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("cowsay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cowsay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
